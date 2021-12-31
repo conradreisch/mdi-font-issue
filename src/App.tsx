67862a2ea9b2478b7e3b1c70d9ccb48c6f5489e4
conradreisch <conrad.reisch@web.de>
2021-12-31 15:03:25 +0100

    switched to sass (scss)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <span className="mdi mdi-chevron-down">Mdi font is working.</span>
+        <p className="test">
+          However, not via CSS pseudo code.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
